6f32be4e764510c9bc0b2711401dcd66060acfff
szrbrhm <81762608+szrbrhm@users.noreply.github.com>
2021-11-01 19:41:19 +0100

    add templates

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Contact
 
 # Register your models here.
+
+class ContactAdmin(admin.ModelAdmin):
+    list_display = (
+        "name", 
+        "email", 
+        "phone_number", 
+        "message", 
+        "created_date"
+    )
+    
+    list_filter = ["name",]
+    search_fields = ["name__startswith",]
+
+admin.site.register(Contact, ContactAdmin)
+
+
