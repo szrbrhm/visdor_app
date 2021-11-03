af08c7fa26b50f5fbbbfe9c80e1759b139c3d021
szrbrhm <81762608+szrbrhm@users.noreply.github.com>
2021-11-03 19:00:43 +0100

    add form

--- home/views.py
+++ home/views.py
@@ -1,10 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from.forms import ContactForm
 
 # Create your views here.
 
 def home(request):
-    return render(request, 'home/index.html')
+    form = ContactForm
+    if request.method == "POST":
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            form.save()
+    
+    context = {
+        "form": form,
+    }
+    
+    return render(request, 'home/index.html', context)
 
 def about(request):
     return render(request, 'home/about.html')
